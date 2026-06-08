7a14834c59df81838137dd87cd792d508ffd246e
goosenotduck <geese@csh.rit.edu>
2026-06-08 16:52:28 -0400

    Add user thingy to navbar

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -1,9 +1,20 @@
+<script setup lang="ts">
+import { authState } from '@/auth';
+
+const user = authState.user
+
+function logout() {
+    window.location.href = "/auth/logout"
+}
+</script>
+
 <template>
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            
+
             <RouterLink class="navbar-brand" to="/">
-                <img class="object-fit-contain ms-2 me-2" height="32" src="https://assets.csh.rit.edu/pubsite/csh_logo_square.svg">
+                <img class="object-fit-contain ms-2 me-2" height="32"
+                    src="https://assets.csh.rit.edu/pubsite/csh_logo_square.svg">
                 Make
             </RouterLink>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
@@ -14,8 +25,31 @@
                 <ul class="navbar-nav mr-auto">
                     <RouterLink class="nav-link" to="/history">History</RouterLink>
                 </ul>
-                <UserMenu />
+                <ul v-if="user" class="nav navbar-nav ml-auto">
+                    <div class="nav-item navbar-user dropdown">
+                        <a class="nav-link dropdown-toggle" id="userDropdownLink" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <img class="rounded-circle pfp me-2" :src="'https://profiles.csh.rit.edu/image/' + user.preferred_username" />
+                            <span class="light-hover me-1">{{ user.name }} </span>
+                            <span class="caret light-hover"></span>
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" @click="logout()">Logout</a>
+                        </div>
+                    </div>
+                </ul>
             </div>
         </div>
     </nav>
 </template>
+
+<style scoped>
+.pfp {
+    width: 2rem;
+    height: 2rem;
+}
+
+.light-hover:hover {
+    color: white;
+}
+</style>
