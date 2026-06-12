4569c556a2569b868101ca4a7dc3cbcf781185a8
goosenotduck <geese@csh.rit.edu>
2026-06-12 15:10:45 -0400

    Split panel by sections

--- web/src/views/admin/AdminDashboardView.vue
+++ web/src/views/admin/AdminDashboardView.vue
@@ -2,27 +2,39 @@
 const sections = [
     {
         title: "Areas",
-        description: "Manage access-controlled areas and their training requirements.",
-        path: "/admin/areas",
-        icon: "bi-door-open"
+        items: [
+            {
+                title: "Areas",
+                description: "Manage access-controlled areas and their training requirements.",
+                path: "/admin/areas",
+                icon: "bi-door-open"
+            },
+            {
+                title: "Create Area",
+                description: "Create a new area.",
+                path: "/admin/areas/create",
+                icon: "bi-plus-circle"
+            }
+        ]
+
     },
     {
-        title: "Create Area",
-        description: "Create a new area.",
-        path: "/admin/areas/create",
-        icon: "bi-plus-circle"
-    },
-        {
-        title: "Create Trainings",
-        description: "Create a new training.",
-        path: "/admin/trainings/create",
-        icon: "bi-plus-circle"
-    },
+        title: "Trainings",
+        items: [
             {
-        title: "Manage User Trainings ",
-        description: "Manage trainings completed by a user",
-        path: "/admin/trainings/create",
-        icon: "bi-plus-circle"
+                title: "Create Trainings",
+                description: "Create a new training.",
+                path: "/admin/trainings/create",
+                icon: "bi-plus-circle"
+            },
+            {
+                title: "Manage User Trainings ",
+                description: "Manage trainings completed by a user",
+                path: "/admin/trainings/create",
+                icon: "bi-plus-circle"
+            },
+        ]
+
     },
 ]
 </script>
@@ -32,22 +44,25 @@ const sections = [
         <div class="container py-4">
             <h1 class="mb-4">Admin Dashboard</h1>
 
-            <div class="row g-4">
-                <div v-for="section in sections" :key="section.path" class="col-md-6 col-lg-4">
-                    <RouterLink :to="section.path" class="text-decoration-none">
-                        <div class="card h-100 shadow-sm">
-                            <div class="card-body">
-                                <h3 class="card-title">
-                                    <i :class="`bi ${section.icon} me-2`"></i>
-                                    {{ section.title }}
-                                </h3>
+            <div>
+                <div v-for="section in sections" class="row g-4 mb-4">
+                    <h1> {{ section.title }}</h1>
+                    <div v-for="item in section.items" :key="section.title" class="col-md-6 col-lg-4 mt-2">
+                        <RouterLink :to="item.path" class="text-decoration-none">
+                            <div class="card h-100 shadow-sm">
+                                <div class="card-body">
+                                    <h3 class="card-title">
+                                        <i :class="`bi ${item.icon} me-2`"></i>
+                                        {{ item.title }}
+                                    </h3>
 
-                                <p class="card-text text-muted">
-                                    {{ section.description }}
-                                </p>
+                                    <p class="card-text text-muted">
+                                        {{ item.description }}
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                    </RouterLink>
+                        </RouterLink>
+                    </div>
                 </div>
             </div>
         </div>
