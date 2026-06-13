49f8e36abbb812f5264f64120c36bbefaa9d5725
goosenotduck <geese@csh.rit.edu>
2026-06-12 22:57:41 -0400

    add background to footer

--- web/src/components/AppFooter.vue
+++ web/src/components/AppFooter.vue
@@ -4,7 +4,7 @@ const year = new Date().getFullYear()
 </script>
 
 <template>
-  <footer class="fixed-bottom text-body-secondary py-2">
+  <footer class="fixed-bottom bg-body text-body-secondary py-2">
     <div class="container d-flex justify-content-center align-items-center gap-3 small">
       <span>
         <i class="bi bi-c-circle me-1"></i>
