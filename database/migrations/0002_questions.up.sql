4f8bc59c7ed976471332c3be732c9c44c96a3d3a
goosenotduck <geese@csh.rit.edu>
2026-06-22 22:26:41 -0400

    prepare for new training system

---
+++ database/migrations/0002_questions.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE training_questions (
+    id SERIAL PRIMARY KEY,
+    training_id INTEGER NOT NULL,
+    label TEXT NOT NULL,
+    type TEXT NOT NULL,
+    required BOOLEAN NOT NULL DEFAULT FALSE,
+
+    FOREIGN KEY (training_id) REFERENCES trainings(id) ON DELETE CASCADE
+);
+
+CREATE TABLE question_options (
+    id SERIAL PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    label TEXT NOT NULL,
+    correct BOOLEAN NOT NULL,
+    position INTEGER NOT NULL,
+    
+    FOREIGN KEY (question_id) REFERENCES training_questions(id) ON DELETE CASCADE
+);
