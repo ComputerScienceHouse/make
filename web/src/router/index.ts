87ab65c976d96774746e3b65d516e831335f1561
goosenotduck <geese@csh.rit.edu>
2026-06-24 16:05:44 -0400

    Add 404 page

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -11,73 +11,67 @@ import TrainingsListView from '@/views/admin/trainings/TrainingsListView.vue'
 import TrainingsEditView from '@/views/admin/trainings/TrainingsEditView.vue'
 import UserTrainingsListView from '@/views/admin/user/UserTrainingsListView.vue'
 import TrainingView from '@/views/TrainingView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
       component: HomeView,
     },
     {
       path: '/areas/:id',
-      name: 'area',
       component: AreaView,
     },
     {
       path: '/training/:id',
-      name: 'training',
       component: TrainingView,
     },
     {
       path: '/admin',
-      name: 'admin',
       component: AdminDashboardView,
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/areas',
-      name: 'areas',
       component: AreaListView,
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/areas/:id',
-      name: 'areaEdit',
       component: AreaEditView,
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/areas/create',
-      name: 'areaCreate',
       component: AreaCreateView,
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/trainings',
-      name: 'trainings',
       component: TrainingsListView,
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/trainings/:id',
-      name: 'trainingsEdit',
       component: TrainingsEditView,
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/trainings/create',
-      name: 'trainingsCreate',
       component: TrainingsCreateView,
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/user/trainings',
-      name: 'userTrainingList',
       component: UserTrainingsListView,
       meta: { requiresAdmin: true },
     },
+    {
+      path: '/:pathMatch(.*)',
+      component: NotFoundView,
+    }
   ],
 })
 
---
+++ web/src/views/NotFoundView.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="container d-flex justify-content-center align-items-center">
+    <div class="bg-text z-n1">404</div>
+
+    <div class="content text-center z-1">
+      <h1 class="fw-bold">Nothing here</h1>
+      <p class="text-break text-muted">
+        Nothing here go away
+      </p>
+
+      <RouterLink to="/" class="btn btn-primary">
+        Return Home
+      </RouterLink>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+main {
+    margin-top: 5rem;
+}
+
+.bg-text {
+  position: absolute;
+  font-size: 18rem;
+  font-weight: 900;
+  opacity: 0.05;
+  user-select: none;
+}
+</style>
