bc59b8a3f995f19091dbd20d8ff1aeb576fdf24d
goosenotduck <geese@csh.rit.edu>
2026-06-24 16:14:16 -0400

    fix admin auth + lazy loading

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,86 +1,72 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AreaView from '@/views/AreaView.vue'
-import AdminDashboardView from '@/views/admin/AdminDashboardView.vue'
-import AreaListView from '@/views/admin/areas/AreaListView.vue'
-import AreaEditView from '@/views/admin/areas/AreaEditView.vue'
 import { authState } from '@/auth.ts'
-import AreaCreateView from '@/views/admin/areas/AreaCreateView.vue'
-import TrainingsCreateView from '@/views/admin/trainings/TrainingsCreateView.vue'
-import TrainingsListView from '@/views/admin/trainings/TrainingsListView.vue'
-import TrainingsEditView from '@/views/admin/trainings/TrainingsEditView.vue'
-import UserTrainingsListView from '@/views/admin/user/UserTrainingsListView.vue'
-import TrainingView from '@/views/TrainingView.vue'
-import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: HomeView,
+      component: () => import('@/views/HomeView.vue'),
     },
     {
       path: '/areas/:id',
-      component: AreaView,
+      component: () => import('@/views/AreaView.vue'),
     },
     {
       path: '/training/:id',
-      component: TrainingView,
+      component: () => import('@/views/TrainingView.vue'),
     },
     {
       path: '/admin',
-      component: AdminDashboardView,
+      component: () => import('@/views/admin/AdminDashboardView.vue'),
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/areas',
-      component: AreaListView,
+      component: () => import('@/views/admin/areas/AreaListView.vue'),
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/areas/:id',
-      component: AreaEditView,
+      component: () => import('@/views/admin/areas/AreaEditView.vue'),
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/areas/create',
-      component: AreaCreateView,
+      component: () => import('@/views/admin/areas/AreaCreateView.vue'),
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/trainings',
-      component: TrainingsListView,
+      component: () => import('@/views/admin/trainings/TrainingsListView.vue'),
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/trainings/:id',
-      component: TrainingsEditView,
+      component: () => import('@/views/admin/trainings/TrainingsEditView.vue'),
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/trainings/create',
-      component: TrainingsCreateView,
+      component: () => import('@/views/admin/trainings/TrainingsCreateView.vue'),
       meta: { requiresAdmin: true },
     },
     {
       path: '/admin/user/trainings',
-      component: UserTrainingsListView,
+      component: () => import('@/views/admin/user/UserTrainingsListView.vue'),
       meta: { requiresAdmin: true },
     },
     {
-      path: '/:pathMatch(.*)',
-      component: NotFoundView,
-    }
-  ],
+      path: '/:pathMatch(.*)*',
+      component: () => import('@/views/NotFoundView.vue'),
+    },
+  ]
 })
 
-router.beforeEach((to, from) => {
+router.beforeEach((to) => {
   // require eboard auth
-  if (to.meta.requiresAuth && !authState.isAdmin()) {
-    return {
-      path: from.fullPath,
-    }
+  if (to.meta.requiresAdmin && !authState.isAdmin()) {
+    return '/'
   }
 })
 
